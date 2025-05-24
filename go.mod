be1b743942e60b35589fece72e29d05e68103f94
Yusuke Hoguro <hoguro4649@yahoo.co.jp>
2025-05-24 16:54:04 +0900

    PostgressSQLの接続を実装

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/yusuke-hoguro/BlogApi
 
 go 1.24.2
+
+require github.com/lib/pq v1.10.9
--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// DB接続情報の取得
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	// Data Souce Nameの設定
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbPassword, dbName)
+
+	// Postgress SQLに接続
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatalf("Failed to connect to DB: %v", err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Failed to connect to DB: %v", err)
+	}
+	fmt.Println("Connected to DB successfully!")
 }
