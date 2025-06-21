d1073a299526cbd686ac0918b84362a2c412a0f5
Yusuke Hoguro <hoguro4649@yahoo.co.jp>
2025-06-21 16:28:09 +0900

    「いいね」を管理するテーブルを追加

--- sql/init.sql
+++ sql/init.sql
@@ -19,3 +19,13 @@ CREATE TABLE comments(
     content TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS likes(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    post_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE(user_id, post_id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE
+);
