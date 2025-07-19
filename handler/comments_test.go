fe2607e069482ced2f0c291b5a5948ef60ae008b
Yusuke Hoguro <hoguro4649@yahoo.co.jp>
2025-07-19 16:04:57 +0900

    コメント削除用API 存在しないIDを指定した場合のテストとJWTトークンが無い場合のテストを追加

--- handler/comments_test.go
+++ handler/comments_test.go
@@ -149,6 +149,87 @@ func TestDeleteCommentHandlerUnauthorized(t *testing.T) {
 
 }
 
+// コメント削除用API 存在しないIDを指定した場合のテストを実施する
+func TestDeleteCommentHandlerNotFound(t *testing.T) {
+	//テスト用DBのセットアップを開始する
+	db := testutils.SetupTestDB(t)
+	defer db.Close()
+
+	//テスト用サーバーのセットアップ
+	server := httptest.NewServer(testutils.SetupTestServer(db))
+	defer server.Close()
+
+	//コメント投稿した人以外のユーザーIDを設定する
+	token, err := handler.GenerateJWT(99)
+	if err != nil {
+		t.Fatal("JWTの生成に失敗", err)
+		return
+	}
+
+	//存在しないコメントIDを指定してJSONデータ作成
+	commentID := 9999
+	url := fmt.Sprintf("%s/comments/%d", server.URL, commentID)
+	req, err := http.NewRequest(http.MethodDelete, url, nil)
+	if err != nil {
+		t.Fatal("リクエスト生成失敗:", err)
+	}
+	req.Header.Set("Authorization", token)
+
+	//リクエスト送信
+	client := server.Client()
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatal("HTTPリクエスト失敗:", err)
+	}
+	defer resp.Body.Close()
+
+	//ステータスコード確認
+	if resp.StatusCode != http.StatusNotFound {
+		t.Errorf("期待するステータスコード %d, 実際は %d", http.StatusNotFound, resp.StatusCode)
+	}
+
+	//ログに表示
+	body, _ := io.ReadAll(resp.Body)
+	t.Logf("レスポンス: %s", string(body))
+
+}
+
+// コメント削除用API JWTトークンが無い場合のテストを実施する
+func TestDeleteCommentHandlerNoAuthorization(t *testing.T) {
+	//テスト用DBのセットアップを開始する
+	db := testutils.SetupTestDB(t)
+	defer db.Close()
+
+	//テスト用サーバーのセットアップ
+	server := httptest.NewServer(testutils.SetupTestServer(db))
+	defer server.Close()
+
+	//コメントIDを指定してJSONデータ作成
+	commentID := 2
+	url := fmt.Sprintf("%s/comments/%d", server.URL, commentID)
+	req, err := http.NewRequest(http.MethodDelete, url, nil)
+	if err != nil {
+		t.Fatal("リクエスト生成失敗:", err)
+	}
+
+	//リクエスト送信
+	client := server.Client()
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatal("HTTPリクエスト失敗:", err)
+	}
+	defer resp.Body.Close()
+
+	//ステータスコード確認
+	if resp.StatusCode != http.StatusUnauthorized {
+		t.Errorf("期待するステータスコード %d, 実際は %d", http.StatusUnauthorized, resp.StatusCode)
+	}
+	//ログに表示
+	body, _ := io.ReadAll(resp.Body)
+	t.Logf("レスポンス: %s", string(body))
+
+}
+
 // 投稿のコメント取得用APIのテスト
 func TestGetCommentsByPostIDHandler(t *testing.T) {
 	// テスト用DBのセットアップを開始する
