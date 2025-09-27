34b3da01a0b2a2e59d396b27654c77a50834cd15
Yusuke Hoguro <hoguro4649@yahoo.co.jp>
2025-09-27 14:12:29 +0900

    Deploy用のymlとシェルを追加

---
+++ utilitys/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+echo "=== Starting deployment ==="
+cd ~/blog-api
+# 最新コードを取得
+git pull origin main
+# Docker 再起動
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
+echo "=== Deployment finished ==="
