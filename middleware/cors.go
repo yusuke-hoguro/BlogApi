e2b161796f9c94837a0ef4fd293125c809e75692
Yusuke Hoguro <hoguro4649@yahoo.co.jp>
2025-10-11 15:55:39 +0900

    サーバー側のCORS設定を修正

--- middleware/cors.go
+++ middleware/cors.go
@@ -9,6 +9,7 @@ func CorsMiddleware(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Origin", "*") // 実環境では任意のドメインにする
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		// プリフライトリクエストへの対応
 		if r.Method == http.MethodOptions {
