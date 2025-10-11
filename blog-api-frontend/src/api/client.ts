157b6d7be1ef191849961b25cd117b9a73abe982
Yusuke Hoguro <hoguro4649@yahoo.co.jp>
2025-10-11 15:55:15 +0900

    開発用と本番環境用でURLを変更する処理を実装

--- blog-api-frontend/src/api/client.ts
+++ blog-api-frontend/src/api/client.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+console.log("VITE_API_BASE_URL =", import.meta.env.VITE_API_BASE_URL);
 const client = axios.create({
-  baseURL: 'http://localhost:8080', // Go APIのベースURL
+  baseURL: import.meta.env.VITE_API_BASE_URL, // Go APIのベースURL
   headers: {
     'Content-Type': 'application/json',
   },
