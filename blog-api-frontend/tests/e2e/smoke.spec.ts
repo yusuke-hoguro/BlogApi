09b4fff0fc9123a94eea1bc3e0efd8971644a92d
Yusuke Hoguro <hoguro4649@yahoo.co.jp>
2025-11-20 19:57:46 +0900

    トップページを開くテストを追加

--- blog-api-frontend/tests/e2e/smoke.spec.ts
+++ blog-api-frontend/tests/e2e/smoke.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('トップページが開ける', async ({ page }) => {
-    await page.goto('/');
-    await expect(page).toHaveTitle(/.*/);
+  await page.goto('http://localhost:3000/');
+  await expect(page).toHaveTitle(/Blog/);
+  await expect(page.getByRole('heading', { name: 'Posts' })).toBeVisible();
 });
