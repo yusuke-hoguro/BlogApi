c8ac44d369fa2c772a28378234c577a934be7fcb
Yusuke Hoguro <hoguro4649@yahoo.co.jp>
2025-11-23 14:57:13 +0900

    Playwriteを使ったテスト用にテストユーザーでログインする関数を追加

---
+++ blog-api-frontend/tests/e2e/utils.ts
@@ -0,0 +1,24 @@
+import { Page } from "@playwright/test";
+
+/*
+* テストユーザーでログインしてTokenを取得する
+* (addInitScriptでLocalStorageにJWTを入れる)
+*/ 
+export async function loginAsTestUser(page: Page) {
+    // テストユーザーでログインしてTokenを取得する
+    const responce = await page.request.post('http://localhost:8080/api/login', {
+        data:{
+            username: "testuser2", 
+            password: "validpassword"
+        }
+    });
+
+    const body = await responce.json();
+    const token = body.token;
+    
+    // addInitScriptは第2引数で渡した値をブラウザ側で実行される関数の第1引数として注入
+    // 第1引数はブラウザ側で実行する関数
+    await page.addInitScript(([jwt]) => {
+        window.localStorage.setItem('token', jwt);
+    }, [token])   
+}
