5486c38ea6be4ce8381ed66cc314b7f4759771cc
Yusuke Hoguro <hoguro4649@yahoo.co.jp>
2025-11-23 14:59:27 +0900

    Playwrite環境確認用にスモークテストファイルを作成

--- blog-api-frontend/tests/e2e/smoke.spec.ts
+++ blog-api-frontend/tests/e2e/smoke.spec.ts
@@ -1,7 +1,15 @@
 import { test, expect } from '@playwright/test';
+import { loginAsTestUser } from './utils';
 
 test('トップページが開ける', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+  // テストユーザーでログインする
+  await loginAsTestUser(page)
+  // トップページにアクセスする
+  await page.goto('http://localhost:3000');
+  // 投稿一覧の見出しが表示されるまで待つ
+  await page.waitForSelector('h1', { timeout: 20000 });
   await expect(page).toHaveTitle(/Blog/);
-  await expect(page.getByRole('heading', { name: 'Posts' })).toBeVisible();
+  // トップページのタイトルを確認する
+  await expect(page.getByRole('heading', { name: '投稿一覧' })).toBeVisible();
 });
+
